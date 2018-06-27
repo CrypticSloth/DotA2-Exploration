6b32a176fe6b2147785b17530c371ca85ae6cfc6
SorensenErik <sorensen.erik48@gmail.com>
2018-06-26 21:52:36 -0500

    Update ward_map.R and added some map pics to test map sizes

--- analysis/TeamWardAnalysis/ward_map.R
+++ analysis/TeamWardAnalysis/ward_map.R
@@ -7,8 +7,7 @@ library(dplyr)
 library(rjson)
 library(png)
 
-setwd("C:/Users/lizar/Google Drive (erik.sorensen20@houghton.edu)/DotaAnalysis/versions_test/7.17")
-json_data <- fromJSON(file="3944571593.json")
+json_data <- fromJSON(file="versions_test/7.17/3944571593.json")
 
 # Get player name
 json_data$players[[1]]$personaname
@@ -17,19 +16,29 @@ json_data$players[[1]]$personaname
 json_data$players[[1]]$obs_log
 json_data$players[[1]]$sen_log
 
-x <- integer()
-y <- integer()
+x_obs <- integer()
+y_obs <- integer()
+x_sent <- integer()
+y_sent <- integer()
 
 for (i in 1:length(json_data$eventData$wardEvents)) {
     if (json_data$eventData$wardEvents[[i]]$x != 0) {
-        x <- c(x,json_data$eventData$wardEvents[[i]]$x)
-        y <- c(y,json_data$eventData$wardEvents[[i]]$y)
+      if (json_data$eventData$wardEvents[[i]]$wardType == 0) {
+        x_obs <- c(x_obs,json_data$eventData$wardEvents[[i]]$x)
+        y_obs <- c(y_obs,json_data$eventData$wardEvents[[i]]$y)
+      } else if (json_data$eventData$wardEvents[[i]]$wardType == 1) {
+        x_sent <- c(x_sent,json_data$eventData$wardEvents[[i]]$x)
+        y_sent <- c(y_sent,json_data$eventData$wardEvents[[i]]$y)
+      }
     }
 }
 
-ima <- readPNG("C:/Users/lizar/Pictures/dota_minimap.png")
-plot(75:190,75:190, type='n')
+ima <- readPNG("analysis/TeamWardAnalysis/detailed_707.png")
+#69:186 ratio does the best for the plot
+sizes <- 69:186
+plot(sizes,sizes, type='n')
 lim <- par()
 rasterImage(ima, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
-points(x,y, col="white")
+points(x_obs,y_obs, col="green")
+points(x_sent,y_sent,col="blue")
    
