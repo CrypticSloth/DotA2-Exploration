e54a6186fcc296758cdcfc1af607780b9782e283
SorensenErik <sorensen.erik48@gmail.com>
2018-07-13 17:08:34 -0500

    added server variable to app

--- apps/app.py
+++ apps/app.py
@@ -319,6 +319,7 @@ fig = go.Figure(data=traces,layout=layout)
 #########################
 
 app = dash.Dash()
+server=app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
