24ec9049267d0d875f91dc47a9fd04dd25c68750
SorensenErik <sorensen.erik48@gmail.com>
2018-10-15 22:44:41 -0400

    added option to save data

--- analysis/collectTeamData.py
+++ analysis/collectTeamData.py
@@ -4,7 +4,7 @@ import os, os.path
 from tqdm import tqdm
 from collections import defaultdict
 
-def search_matches_for_team(target_team, target_versions, progress_bar = False, path='../versions_test'):
+def search_matches_for_team(target_team, target_versions, , collect_data = False, progress_bar = False, path='../versions_test'):
     """
     Loop through data files and collect only the match IDs that have a certain team in it
     and return a dictionary of lists that contains the matches that the team played under either radiant or dire
@@ -25,7 +25,39 @@ def search_matches_for_team(target_team, target_versions, progress_bar = False, 
     Make it have two options, one to either return a list of ids of games the target team played in, or return a list of the actual games data
     """
 
+    
     target_matches = defaultdict(list)
+    target_data = defaultdict(list)
+
+    if collect_data == True:
+        if progress_bar == True:
+            for version in target_versions:
+                for match in tqdm([name for name in os.listdir('{:}/{:}'.format(path, version))],desc="gathering data from version {:}".format(version)):
+                    with open('{:}/{:}/{:}'.format(path, version, match),'r') as outfile:
+                        data = json.load(outfile)
+                    try:
+                        if data['radiant_team']['name'] == target_team: 
+                            target_data['radiant'].append(data)
+                        if data['dire_team']['name'] == target_team:
+                            target_data['radiant'].append(data)
+                    except:
+                        continue  
+            return(target_data)
+
+        if progress_bar == False:
+            for version in target_versions:
+                for match in [name for name in os.listdir('{:}/{:}'.format(path, version))]:
+                    with open('{:}/{:}/{:}'.format(path, version, match),'r') as outfile:
+                        data = json.load(outfile)
+                    try:
+                        if data['radiant_team']['name'] == target_team: 
+                            target_data["radiant"].append(data)
+                        if data['dire_team']['name'] == target_team:
+                            target_data["dire"].append(data)
+                    except:
+                        continue
+            return(target_data)
+
 
     if progress_bar == True:
         for version in target_versions:
