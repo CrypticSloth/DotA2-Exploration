dd51795399b3c22f20b6869ace9e10a21a3e0840
CrypticSl0th <sorensen.erik48@gmail.com>
2018-10-29 11:22:53 -0400

    Padding works (at first glance) but each player is still uneven

---
+++ analysis/TeamMovementAnalysis/2dMotionPath.py
@@ -0,0 +1,135 @@
+# See what we can do with a 2d graph of motion data
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from operator import itemgetter
+sys.path.append('C:/GitHub/DotA2-Exploration/analysis')
+from collectTeamData import search_matches_for_team
+data_path = 'C:/GitHub/DotA2-Exploration/versions_test/'
+
+# Get data
+target_team = "Evil Geniuses"
+target_versions = ["7.18"]
+matches = search_matches_for_team(target_team, target_versions, progress_bar = True, path=data_path)
+
+# Filter out motion data
+motion_data = []
+names = []
+
+for i in range(5):
+    motion_data.append(matches['radiant'][0]['players'][i]['eventData']['playerUpdatePositionEvents'])
+    names.append(matches['radiant'][0]['players'][i]['name'])
+
+# matches['radiant'][0]['match_id']
+# sum([x['timeDead'] for x in matches['radiant'][0]['players'][0]['eventData']['deathEvents']])
+# matches['radiant'][0]['players'][0]['eventData']['deathEvents']
+# matches['radiant'][0]['duration'] + 89
+# matches['radiant'][0]['players'][0]['eventData']['playerUpdatePositionEvents']
+
+# Need to pad the data for when the player is not moving, as it does not record the time when the player is not moving
+# This also happens when they are dead, the team jumps say 30 seconds when the player dies
+# to fix this, detect whenever the time skips, and then use the most recent motion update coordinates for each missing time
+
+padded_player_pos = []
+for player_i in range(5):
+    playerPosition = matches['radiant'][0]['players'][player_i]['eventData']['playerUpdatePositionEvents']
+    for i in range(len(playerPosition)):
+        # if next index is not one more than the previous time, then make that time have the same x and y as the most recent one
+        now = playerPosition[i]
+        next = playerPosition[i+1]
+        if (now['time'] != (next['time'] + 1)):
+            for x in range(next['time'] - now['time'] - 1):
+                playerPosition.append({'time':(now['time'] + add), 'x':now['x'], 'y':now['y']})
+                add += 1
+            add = 1
+    newPlayerPos = sorted(playerPosition, key=itemgetter('time'))
+    padded_player_pos.append(newPlayerPos)
+
+len(padded_player_pos)
+len(padded_player_pos[0])
+len(padded_player_pos[1])
+len(padded_player_pos[2])
+len(padded_player_pos[3])
+len(padded_player_pos[4])
+
+padded_player_pos[0][-1]
+padded_player_pos[1][-1]
+padded_player_pos[2][-1]
+padded_player_pos[3][-1]
+padded_player_pos[4][-1]
+
+# Build the data frame
+len(motion_data[1])
+names
+
+df = pd.DataFrame()
+
+time = []
+for i in motion_data[0]:
+    time.append(i['time'])
+
+df['time'] = time
+for i in range(len(names)):
+    x = []
+    y = []
+    for moment in motion_data[i]:
+        time.append(moment['time'])
+        x.append(moment['x'])
+        y.append(moment['y'])
+    df['{}_x'.format(names[i])] = x # Each player does not have the same amount of data (find the player with the most data and pad the rest of the players with 0's)
+    df['{}_y'.format(names[i])] = y
+    df['{}_xy'.format(names[i])] = np.array(x) + np.array(y)
+
+df
+
+
+
+# Plot it ######################
+
+import plotly as py
+import plotly.graph_objs as go
+
+# Create random data with numpy
+import numpy as np
+
+N = 100
+random_x = np.linspace(0, 1, N)
+random_y0 = np.random.randn(N)+5
+random_y1 = np.random.randn(N)
+random_y2 = np.random.randn(N)-5
+
+# Create traces
+trace0 = go.Scatter(
+    x = random_x,
+    y = random_y0,
+    mode = 'lines',
+    name = 'lines'
+)
+trace1 = go.Scatter(
+    x = random_x,
+    y = random_y0,
+    mode = 'lines',
+    name = 'lines'
+)
+trace2 = go.Scatter(
+    x = random_x,
+    y = random_y0,
+    mode = 'lines',
+    name = 'lines'
+)
+trace3 = go.Scatter(
+    x = random_x,
+    y = random_y0,
+    mode = 'lines',
+    name = 'lines'
+)
+trace4 = go.Scatter(
+    x = random_x,
+    y = random_y0,
+    mode = 'lines',
+    name = 'lines'
+)
+data = [trace0, trace1, trace2, trace3, trace4]
+
+py.offline.plot(data, filename='line-mode')
