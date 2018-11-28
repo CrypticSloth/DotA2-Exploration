e19c82a1be3acf8387356ed470e4e21b5bef9c94
CrypticSl0th <sorensen.erik48@gmail.com>
2018-11-27 22:41:19 -0500

    added Omegamans (from the glimpse podcast) notes

--- tournamentGoldPercApp/findPlayerGoldPerc.py
+++ tournamentGoldPercApp/findPlayerGoldPerc.py
@@ -1,6 +1,14 @@
 # make searchable by match id (from tourneys) the player gold perc of the teams perc at every time stamp
 # Need to get the data in a dataframe where the gold for each player is there along with the time stamp and gold perc at that time stamp
 
+# Note from Omegaman on what he wants
+'''
+1. percent of team's networth at any point in the game. Datdota has the raw values for each players networth at minute intervals. I want as percent of the team. I can do this manually in excel, but it takes me maybe half an hour per tournament to put it together
+2. Basically all stats from datdota's "frames" available in a winning or losing format. In other words I want to be able to look at what a team or players networth/kills etc are in a win vs a loss
+The questions i want to be able to ask are like "On NIP who is the player who if they have a bad first 10 minutes is most likely to lead to their team struggling?" so if Ace averages 4k networth at 10 minutes in wins and 3.8k networth in losses, but Fata averages 4k networth in wins and 3k in losses it appears like Fata is the key to their early game
+That one sounds a lot hard
+'''
+
 import requests
 import numpy as np
 import pandas as pd
@@ -50,5 +58,6 @@ def collect_match_data(ID = 4238597779):
                 net_worth.append(match['players'][p]['eventData']['playerUpdateGoldEvents'][x]['networth'])
         df['{}_gold'.format(match['players'][p]['name'])] = gold
         df['{}_networth'.format(match['players'][p]['name'])] = net_worth
+        # Add player percentage of net worth to the data frame
 
     return df
