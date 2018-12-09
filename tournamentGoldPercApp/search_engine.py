7a50812c5d0c66af349d9a0933575982a558e8ae
SorensenErik <sorensen.erik48@gmail.com>
2018-12-09 12:04:29 -0500

    Added appTest.py and its dependencies to run redis server backend for long processes

---
+++ tournamentGoldPercApp/search_engine.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Dummy Search Engine
+
+# Wiley Intelligent Solutions, 2018
+# John Wiley & Sons, Inc.
+
+# coding: utf-8
+
+import time
+
+def run_query(duration, string):
+	"""
+	All this does is sleep for duration.  Add your own database queries, SKLearn magic,
+	kitchen prose and gutter rhymes here.
+	"""
+	time.sleep(duration)
+	return '4000 holes in {}'.format(string)
