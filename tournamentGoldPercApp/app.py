ed886916dd3fbe738a2359c2e2495a1d9eb16536
CrypticSl0th <sorensen.erik48@gmail.com>
2018-12-17 20:46:54 -0500

    Works. Added table display to app

--- tournamentGoldPercApp/app.py
+++ tournamentGoldPercApp/app.py
@@ -16,6 +16,17 @@ from findPlayerGoldPerc import *
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+def generate_table(dataframe, max_rows=1):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(len(dataframe))]
+    )
+
 # ID = 4247904407
 # plot = create_plots(collect_match_data(ID),'10:0')
 #
@@ -27,6 +38,10 @@ server = app.server
 
 app.layout = html.Div([
     html.H2('Players Net Worth Percentage'),
+    html.H3('Instructions:'),
+    html.Div('Put the match ID you are interested getting stats from in the first slot and put the time into the second slot.'),
+    html.Div('Make sure the time is in the format like how you see it in game (MM:SS). For example: 10:35.'),
+    html.Div('When you are ready, click RUN! '),
     dcc.Input(id='input-id', type='text', value= "Match ID"),
     dcc.Input(id='input-time', type='text', value='Time'),
     html.Button('Run', id='button'),
@@ -35,7 +50,8 @@ app.layout = html.Div([
         # figure={
         #     'data': create_plots(collect_match_data(4223661333),'10:0')
         # }
-    )
+    ),
+    html.Div([html.Table(id='my-table')], style={'width': '25%','display': 'inline-block', 'padding': '0 20'})
 ])
 
 # @app.callback(
@@ -56,8 +72,21 @@ app.layout = html.Div([
 def update_value(n_clicks,value_id,value_time):
     print("running")
     if n_clicks > 0:
+        df = faster_collect_match_data(value_id,value_time)
 
-        return create_plots_fast(faster_collect_match_data(value_id,value_time))
+        return create_plots_fast(df)
+
+@app.callback(
+     Output(component_id='my-table',component_property='children'),
+     [Input('button','n_clicks')],
+     [State('input-id', 'value'),
+     State('input-time', 'value')])
+def update_value(n_clicks,value_id,value_time):
+    print("running table")
+    if n_clicks > 0:
+        df = faster_collect_match_data(value_id,value_time)
+        return generate_table(df)
+
     # print(ns1)
     # print(nb1)
     # print(ns2)
