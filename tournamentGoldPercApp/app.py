7f06aa2c82a7f49b8f95de7524a5cc84a7e28e83
Erik Sorensen <sorensen.erik48@gmail.com>
2019-06-24 18:34:58 -0500

    Removed unessessary css. Added Bootstrap 4 external stylesheet

--- tournamentGoldPercApp/app.py
+++ tournamentGoldPercApp/app.py
@@ -2,12 +2,11 @@ import os
 # os.chdir('C:/GitHub/DotA2-Exploration/tournamentGoldPercApp') # for atom hydrogen
 import plotly
 import dash
+import dash_bootstrap_components as dbc # For loading bars
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from findPlayerGoldPerc import *
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 def generate_table(dataframe, max_rows=1):
     return html.Table(
@@ -19,6 +18,15 @@ def generate_table(dataframe, max_rows=1):
             html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
         ]) for i in range(len(dataframe))]
     )
+
+external_stylesheets = [
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+        'rel':'stylesheet',
+        'integrity': 'sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T',
+        'crossorigin': 'anonymous'
+    }
+]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -43,10 +51,19 @@ app.layout = html.Div([
             html.Br(),
             'Simply enter in the id of the game you would like to analyze and click RUN!'
         ]),
-
-        dcc.Input(id='input-id-large', type='text', placeholder="Match ID", value="4223661333"),
-        # dcc.Input(id='input-time', type='text', placeholder='Time',value=''),
-        html.Button('Run', id='button_large', type='submit'),
+        html.Div(className='container',children=[
+            html.Div(className='row justify-content-start', children=[
+                html.Div(className='col align-self-start',children=[
+                    dcc.Input(className='form-control',id='input-id-large', type='text', placeholder="Match ID", value="4223661333"),
+                ]),
+                html.Div(className='col align-self-start',children=[
+                    html.Button('Run', className='btn btn-primary', id='button_large', type='submit'),
+                ]),
+                html.Div(className='col align-self-start',children=[
+                    dcc.Loading(id="loading-1", children=[html.Div(id="loading-output-1")], type="default", fullscreen=False),
+                ])
+            ])
+        ]),
 
         # status infomation, e.g. "please wait"
         html.Div(id='status'),
@@ -110,13 +127,15 @@ def update_value(n_clicks,value_id,value_time):
         return generate_table(df)
 
 @app.callback(
-    Output(component_id='large-results',component_property='figure'),
+    [Output(component_id='large-results',component_property='figure'),
+    Output("loading-output-1", "children")],
     [Input('button_large','n_clicks')],
     [State('input-id-large','value')])
 def plot_large(n_clicks,value_id):
     print("running large graph")
     if n_clicks > 0:
-        return plot_perc_networth_overtime(value_id, 30) # Set to 30 to make runtime much faster
+        return plot_perc_networth_overtime(value_id, 30),None # The none is there for the second output which controls the loading bar
+        # return plot_perc_networth_overtime(value_id, 30), None # The none is there for the second output which controls the loading bar
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
